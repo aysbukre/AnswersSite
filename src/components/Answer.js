f4561ae17fa5f06dd35a7545f5471ac7bc9db5f6
aysbukre <aysbukre@gmail.com>
2024-08-27 20:26:03 +0300

    answer page

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import seaImage from '../images/sea.png'
 
 const Answer = () => {
 
 
     return (
-        <div className="answer">
+        <div className="answer" style={{
+            backgroundImage: `url(${seaImage})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: 'white',
+        }}>
             <h1>Bu sorunun cevabı  kalbinde.</h1>
 
         </div>
